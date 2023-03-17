ade9d3a0bfc4ed251309168812cafabac70cb0a1
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-03-17 11:08:35 +0100

    add:getter and setter to class Event

--- src/org/test/java/Event.java
+++ src/org/test/java/Event.java
@@ -3,24 +3,54 @@ package org.test.java;
 import java.time.LocalDate;
 
 public class Event {
-
+//      Attributes
     private int capacity;
     private String title;
     private LocalDate date;
     private int reservationNumber;
 
-    public Event(int capacity, String title, LocalDate date) {
+//     Constructor
+//    DO COSTUM EXCEPTION
+    public Event(int capacity, String title, LocalDate date) throws RuntimeException {
         if(date.isBefore(LocalDate.now())){
             throw new RuntimeException();
         }
         if (capacity<=0){
             throw new RuntimeException();
         }
+        date.format()
         this.capacity = capacity;
         this.title = title;
         this.date = date;
         reservationNumber = 0;
     }
+
+//    Getter and Setter
+    public int getCapacity() {
+        return capacity;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getReservationNumber() {
+        return reservationNumber;
+    }
+//    Methods
 
 
 }
